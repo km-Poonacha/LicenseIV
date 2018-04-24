61a7386eb0c283afe2a71ba96a0a674e7b87c5ff
km-poonacha <km.poonacha@gmail.com>
2018-04-24 11:13:28 +0200

    Added the first py file

---
+++ GetSCLicense.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Code for developing the network for n-stage least square. 
+Step 1: Find the repos started by the owner of the projects. Only individual owned projects 
+Step 2: See which of the repos are forks started prior to 2014
+Step 3: Find the license of the forked repositories
+Step 4: Find the likelihood of choice of license
+"""
