0de551a330f0f2654ccec9e9c43d52c189468c64
km-poonacha <km.poonacha@gmail.com>
2018-04-29 23:11:35 +0200

    Facing issues with space at the end of csv files.. Cretaed issues

--- GetSCLicenseLvl2.py
+++ GetSCLicenseLvl2.py
@@ -7,6 +7,8 @@ Step 1: Find the repos started by the owner of the projects. Only individual own
 Step 2: See which of the repos are forks started prior to 2014
 Step 3: Find the license of the forked repositories
 Step 4: Find the likelihood of choice of license
+
+ FACED MAJOR ISSUES WITH A SPACE BEING ADDED AT THE END OF SOME OF THE CSV FILES . NOT SURE WHY
 """
 
 import csv
